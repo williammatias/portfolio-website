4d604850ba96e0385293dbae2a53d15212dd1ab0
Qualixium <matias.b.william@gmail.com>
2020-03-22 12:17:52 -0400

    adding gulp task and scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,42 @@
-var connect = require("gulp-connect");
-const { watch, task, parallel, series, src, dest } = require("gulp");
+const { watch, task, parallel, series, src, dest } = require("gulp"),
+  connect = require("gulp-connect"),
+  log = require("fancy-log"),
+  gulpif = require("gulp-if"),
+  minifyHTML = require("gulp-minify-html");
+let sass = require("gulp-sass");
+sass.compiler = require("node-sass");
 
-task("check", function() {
- watch(["./src/*.html"], series("html"));
+let env, jsSources, sassSources, outputDir = 'build';
+
+env = process.env.NODE_ENV || "development";
+
+
+sassSources = ["src/assets/scss/style.scss"];
+
+task("watch", function() {
+  watch(["src/*.html"], series("html"));
+  watch(["src/assets/scss/*.scss"], series("scss"));
 });
 
 task("connect", function() {
   connect.server({
-    root: "",
+    root: outputDir,
     livereload: true
   });
 });
 
 task("html", function() {
   return src("./src/*.html")
+    .pipe(gulpif(env === "production", minifyHTML()))
+    .pipe(dest(outputDir))
     .pipe(connect.reload());
 });
 
-exports.default = parallel("connect", "check");
+task("scss", function() {
+  return src(sassSources)
+    .pipe(sass().on("error", log))
+    .pipe(dest(outputDir + "/css"))
+    .pipe(connect.reload());
+});
+
+exports.default = parallel("connect", "scss", "html", "watch");
